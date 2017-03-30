ab90c48383fbc04ea11d2d52acaf814e2ec66e77
Amos <amosgathungu@gmail.com>
2017-03-30 14:54:36 -0400

    class excercise

--- music.sql
+++ music.sql
@@ -22,3 +22,13 @@ CREATE TABLE song(
     duration numeric,
     year integer
 );
+
+select * from track where aid = 4;
+
+select * from album where mid = 2;
+
+select * from track order by duration desc limit 1;
+
+select * from album where year between 1960 and 2000 order by year;
+
+select * from album where year between 1990 and 1999 and mid = 2;
